d79ede3649209f4974ec04b1c231a180b53f9092
FireML <fireml@firedev.uk>
2026-04-22 23:20:27 +0100

    Use ItemBuilder for player head drops

--- src/main/java/uk/firedev/datapackssuck/listeners/PlayerHeadDrops.java
+++ src/main/java/uk/firedev/datapackssuck/listeners/PlayerHeadDrops.java
@@ -1,17 +1,12 @@
 package uk.firedev.datapackssuck.listeners;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ItemType;
+import uk.firedev.daisylib.builders.ItemBuilder;
 import uk.firedev.datapackssuck.config.MainConfig;
-
-import java.util.List;
 
 public class PlayerHeadDrops implements Listener {
 
@@ -25,16 +20,9 @@ public class PlayerHeadDrops implements Listener {
         if (killer == null) {
             return;
         }
-        ItemStack head = ItemType.PLAYER_HEAD.createItemStack(
-            skullMeta -> {
-                skullMeta.setPlayerProfile(player.getPlayerProfile());
-                skullMeta.lore(List.of(
-                    Component.text("Killed by " + killer.getName())
-                        .color(NamedTextColor.YELLOW)
-                        .decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE)
-                ));
-            }
-        );
+        ItemStack head = ItemBuilder.skull(player)
+            .addLore("<yellow>Killed by " + killer.getName())
+            .getItem();
         player.getWorld().dropItemNaturally(player.getLocation(), head);
     }
 
